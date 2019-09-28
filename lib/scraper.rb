30a456d23e3f440f9853f890ae1d726a16fdc0b4
Michael Lynch <michael@michaelrlynch.com>
2019-09-28 00:44:02 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,7 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+html = open("https://www.gcplsoccer.com/2019-standings/")
+
+doc = Nokogiri::HTML(html)
+puts doc
